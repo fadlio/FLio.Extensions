41fa84c4a6e0db04447747a3bc7e6eb8d2d5fe05
Fernando de Lio <fernandodelio@gmail.com>
2022-06-13 13:14:54 -0700

    Initial commit

---
+++ ConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace FLio.Extensions;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AddJsonEmbedded(this IConfigurationBuilder builder, string path)
+    {
+        var assembly = typeof(ConfigurationBuilderExtensions).Assembly;
+        var resourceNames = assembly.GetManifestResourceNames();
+        var resourceName = resourceNames.FirstOrDefault(n => n.Contains(path));
+        if (resourceName == null)
+            throw new FileNotFoundException();
+
+        var assemblyStream = assembly.GetManifestResourceStream(resourceName)!;
+        builder.AddJsonStream(assemblyStream);
+        return builder;
+    }
+}
---
+++ ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace FLio.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddOptionsWithValidation<T>(
+        this IServiceCollection services,
+        HostBuilderContext builder
+    ) where T : class
+    {
+        services
+            .AddOptions<T>()
+            .Bind(builder.Configuration.GetSection(typeof(T).Name[..^7]))
+            .ValidateDataAnnotations();
+
+        // Explicitly register the settings object by delegating to the IOptions object and performs initial validation
+        services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<T>>().Value);
+    }
+}
